3da5f92e829aebf3731e3046286a52e5d1c6729a
Heather-Ridgill <Heather.Ridgill@gmail.com>
2020-06-23 17:07:57 -0700

    Looks more like game of life with moving pieces!

--- pygameoflife/gameoflife.py
+++ pygameoflife/gameoflife.py
@@ -6,20 +6,47 @@ import pygame
 import pygame.draw
 import random
 import time
+import tkinter as tk
 
 
-BOARD_SIZE = width, height = 640, 480
+
+def onclick(args):
+    if args == 1:
+        print("Start Animation")
+    if args == 2:
+        print("Stop Animation")
+    if args == 3:
+        print("Clear")
+
+root = tk.Tk()
+
+
+btn1 = tk.Button(root, text="Start Animation", command=lambda:onclick(1))
+btn2 = tk.Button(root, text="Stop Animation", command=lambda:onclick(2))
+btn3 = tk.Button(root, text="Clear Grid", comman=lambda:onclick(3))
+
+# To put element in window
+btn1.pack()
+btn2.pack()
+btn3.pack()
+
+root.mainloop()
+
+BOARD_SIZE = width, height = 840, 620
 BOX_SIZE = 10
-# speed = [2, 2]
+# speed = [4, 4]
 DEAD_COLOR = 0, 0, 0
 ALIVE_COLOR = 255, 0, 0
 #FPS = Frames per sec, how fast does the grid move randomly?
-MAX_FPS = 8
+MAX_FPS = 20
+
+
 
 
 class GameOfLife:
 
     def __init__(self):
+
         pygame.init()
         self.screen = pygame.display.set_mode(BOARD_SIZE)
         self.clear_screen()
@@ -55,14 +82,14 @@ class GameOfLife:
     # set_grid(1) all alive
     # set_grid() Random
     # set_grid() Random
-    def set_grid(self, value=None):
+    def set_grid(self, value=None, grid = 0):
         for r in range(self.num_rows):
             for c in range(self.num_cols):
                 if value is None:
                     cell_value = random.randint(0, 1)
                 else:
                     cell_value = value
-                self.grids[self.game_grid_active][r][c] = cell_value
+                self.grids[grid][r][c] = cell_value
 
                 
     def draw_grid(self):
@@ -81,52 +108,53 @@ class GameOfLife:
     def clear_screen(self):
         self.screen.fill(DEAD_COLOR)
 
+    def get_cell(self, r, c):
+        try:
+            cell_value = self.grids[self.game_grid_active][r][c]
+        except:
+            cell_value = 0
+        return cell_value
+    
+    
     def check_cell_neighbors(self, row_index, col_index):
         # Get the number of alive cells surrounding current cell
-
         # implent the 4 rules: too populated, underpopulated, death, birth
-        # self.grids[self.game_grid_active][r][c] # Current Cell
-
         #check all 8 neighbors, add up alive count
- 
-        def get_cell(r, c):
-            try:
-                cell_value = self.grids[self.game_grid_active][r][c]
-            except:
-                cell_value = 0
-            return cell_value
 
         num_alive_neighbors = 0
+        num_alive_neighbors += self.get_cell(row_index - 1, col_index - 1)
+        num_alive_neighbors += self.get_cell(row_index - 1, col_index)
+        num_alive_neighbors += self.get_cell(row_index - 1, col_index + 1)
+        num_alive_neighbors += self.get_cell(row_index, col_index - 1)
+        num_alive_neighbors += self.get_cell(row_index, col_index + 1)
+        num_alive_neighbors += self.get_cell(row_index + 1, col_index - 1)
+        num_alive_neighbors += self.get_cell(row_index + 1, col_index)
+        num_alive_neighbors += self.get_cell(row_index + 1, col_index + 1)
 
-        num_alive_neighbors += get_cell(row_index - 1, col_index - 1)
-        num_alive_neighbors += get_cell(row_index - 1, col_index)
-        num_alive_neighbors += get_cell(row_index - 1, col_index + 1)
-        num_alive_neighbors += get_cell(row_index, col_index - 1)
-        num_alive_neighbors += get_cell(row_index, col_index + 1)
-        num_alive_neighbors += get_cell(row_index + 1, col_index - 1)
-        num_alive_neighbors += get_cell(row_index + 1, col_index)
-        num_alive_neighbors += get_cell(row_index + 1, col_index + 1)
+        # print(num_alive_neighbors)
+        # print(self.game_grid_active, row_index, col_index, self.num_cols, self.num_rows)
 
-        print(num_alive_neighbors)
+        # RULES
 
+        if self.grids[self.game_grid_active][row_index][col_index] == 1: #alive
+            if num_alive_neighbors > 3: # 2.) Overpopulation
+                return 0
+            if num_alive_neighbors < 2: # 3.) Underpopulation
+                return 0
+            if num_alive_neighbors == 2 or num_alive_neighbors == 3:
+                return 1
+        elif self.grids[self.game_grid_active][row_index][col_index] == 0: # dead
+            if  num_alive_neighbors == 3:
+                return 1 # 4.) Come to life
 
-        # come to life
-        if self.grids[self.game_grid_active][row_index][col_index] == 0 and num_alive_neighbors == 3:
-            return 1  
-        if num_alive_neighbors > 4: # Overpopulation
-            return 0
-        if num_alive_neighbors < 3: # Underpopulation
-            return 0
-        if (num_alive_neighbors == 3 or num_alive_neighbors) == 4 and self.grids[self.game_grid_active][row_index][col_index] == 1:
-            return 1
-
+        return self.grids[self.game_grid_active][row_index][col_index]
 
 
     def update_generation(self):
-        # TODO
         # Inspect the current active
-        for r in range(self.num_cols):
-            for c in range(self.num_rows):
+        self.set_grid(0, self.inactive_grid())
+        for r in range(self.num_rows - 1):
+            for c in range(self.num_cols - 1):
               next_gen_state = self.check_cell_neighbors(r, c)
               # Set the inactive grid future cell state
               self.grids[self.inactive_grid()][r][c] = next_gen_state
@@ -160,10 +188,7 @@ class GameOfLife:
 
     def cap_frame_rate(self):
         now = pygame.time.get_ticks()
-        print("Now: " + str(now))
         milliseconds_since_last_update = now - self.last_update_completed
-        print(milliseconds_since_last_update)
-
 
         time_to_sleep = self.desired_milliseconds_between_updates - milliseconds_since_last_update
         if time_to_sleep > 0:
